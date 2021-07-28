666b4dab4b3582f58eec7fd26cac8eab26c1d05f
sorojasp <stivenorlandorojaspulido@gmail.com>
2021-07-28 10:20:08 -0500

    update the project

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 
-@Component({
+@Component({ // this a decorator that add a component properties to a Tab1Page class
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
